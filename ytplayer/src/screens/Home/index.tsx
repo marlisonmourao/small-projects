9a55568cc9cc4293d4741932e49fb4bd6c21ce55
marlisonmourao <bentesmourao@gmail.com>
2023-01-31 19:40:11 -0400

    leaving the video in full screen

--- ytplayer/src/screens/Home/index.tsx
+++ ytplayer/src/screens/Home/index.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
-import { View, ActivityIndicator, useWindowDimensions } from 'react-native';
-
-import YoutubeIframe from 'react-native-youtube-iframe';
+import { useCallback, useState } from 'react';
+import { View, ActivityIndicator, useWindowDimensions, Alert } from 'react-native';
+import YoutubeIframe, { PLAYER_STATES } from 'react-native-youtube-iframe';
+import  * as ScreenOrientation from 'expo-screen-orientation'
 
 import { styles } from './styles';
 import { VIDEO_HEIGHT, SCREEN_SPACE } from './styles';
@@ -12,6 +12,20 @@ export function Home() {
   const { width } = useWindowDimensions()
   const VIDEO_WIDTH = width - (SCREEN_SPACE * 2)
 
+  const onFullScreenChange = useCallback((isFullScreen: boolean) => {
+    if(isFullScreen) {
+      ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE)
+    } else {
+      ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT)
+    }
+  }, [])
+
+  const onChangeState = useCallback((state: string) => {
+    if(state === PLAYER_STATES.ENDED) {
+      Alert.alert('E ai, curtiu?', 'Não deixe de curtir, comentar e seguir o canal')
+    }
+  }, [])
+
   return (
     <View style={styles.container}>
       <View style={styles.player}>
@@ -20,6 +34,8 @@ export function Home() {
           width={VIDEO_WIDTH}
           height={videoReady ? VIDEO_HEIGHT : 0}
           onReady={() => setVideoReady(true)}
+          onFullScreenChange={onFullScreenChange}
+          onChangeState={onChangeState}
         />
 
         {!videoReady && <ActivityIndicator color="red" />}
