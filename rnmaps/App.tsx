23bf4cf54e01f0a45a7a251ed9acf9e615501eb1
marlisonmourao <bentesmourao@gmail.com>
2023-02-17 15:14:34 -0400

    getting current location of user

--- rnmaps/App.tsx
+++ rnmaps/App.tsx
@@ -1,10 +1,60 @@
+import { useEffect, useState } from 'react';
 import { View } from 'react-native';
 
-import { styles } from './styles'
+import { requestForegroundPermissionsAsync, getCurrentPositionAsync, LocationObject, watchPositionAsync, LocationAccuracy } from 'expo-location'
+import MapView, { Marker } from 'react-native-maps'; 
+import { styles } from './styles';
+
 
 export default function App() {
+  const [location, setLocation] = useState<LocationObject | null>(null);
+
+  async function requestForegroundPermissions() {
+    const { granted } = await requestForegroundPermissionsAsync()
+
+    if(granted) {
+      const currentPosition = await getCurrentPositionAsync()
+      setLocation(currentPosition)
+    }
+  }
+
+  useEffect(() => {
+    requestForegroundPermissions()
+  }, [])
+
+  useEffect(() => {
+    watchPositionAsync({
+      accuracy: LocationAccuracy.Highest,
+      timeInterval: 1000,
+      distanceInterval: 1
+    }, (response) => {
+      console.log('NOVA LOCALIZAÇÃO:', response);
+      setLocation(response)
+    })
+  }, [])
+
   return (
     <View style={styles.container}>
+
+    {
+      location &&
+      <MapView 
+        style={styles.map}
+        initialRegion={{
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
+        }}
+      >
+        <Marker 
+          coordinate={{
+            latitude: location.coords.latitude,
+             longitude: location.coords.longitude,
+          }}
+        />
+      </MapView>
+    }
 
     </View>
   );
