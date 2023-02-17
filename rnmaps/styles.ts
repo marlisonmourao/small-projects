9a9ea4239fe6f6755016526b30188ca77d347599
marlisonmourao <bentesmourao@gmail.com>
2023-02-17 15:14:43 -0400

    rnmaps

--- rnmaps/styles.ts
+++ rnmaps/styles.ts
@@ -3,8 +3,12 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#000",
     alignItems: "center",
     justifyContent: "center",
   },
+  map: {
+    flex: 1,
+    width: "100%",
+  }
 });
