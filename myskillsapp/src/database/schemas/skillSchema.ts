a3c7ab1277c2d7223dcf1473a4915e1f176007b1
marlisonmourao <bentesmourao@gmail.com>
2023-06-27 23:46:09 -0400

    innital commit

---
+++ myskillsapp/src/database/schemas/skillSchema.ts
@@ -0,0 +1,9 @@
+import { tableSchema  } from '@nozbe/watermelondb'
+
+export const skillSchema = tableSchema({
+  name: 'skills',
+  columns: [
+    {name: 'name', type: 'string'},
+    {name: 'type', type: 'string'},
+  ]
+})
